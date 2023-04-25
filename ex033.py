b0a364bf51bd0d0ef1e9ab2ff73003a035161db3
João Victor <128199213+Jo4aV1ctor@users.noreply.github.com>
2023-04-25 10:52:04 -0300

    Atualizar o ex033.py

--- ex033.py
+++ ex033.py
@@ -13,21 +13,3 @@ while True:
         break
   except ValueError:
     print('Por favor digite um numero valido!')
-
-'''
-while True:
-    try:
-        lista = []
-        limite = 3
-        for i in range(limite):
-            numero = int(input('Digite um numero: '))
-            lista.append(numero)
-        numero_maior = max(lista)
-        numero_menor = min(lista)
-        print(f'O maior numero é o {numero_maior}!\nE o menor numero é o {numero_menor}!')
-        resposta = input('Gostaria de verificar outros numeros? (s/n)')
-        if resposta == "n":
-            break
-    except ValueError:
-        print('Por favor, digite um número válido!')
-'''
